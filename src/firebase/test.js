22648a72e16e0b36234850c3886a84fcb045b77e
Samed Yalniz <yalniz.samed@gmail.com>
2020-09-15 14:07:38 +0300

    added ability to store authenticated users to firebase database

---
+++ src/firebase/test.js
@@ -0,0 +1,8 @@
+import firebase from 'firebase/app';
+
+import 'firebase/firestore';
+
+const firestore = firebase.firestore();
+
+firestore.collection('users').doc('4qTh9sy5kMnBfIev13xW').collection('cartItems').doc('aqZmlhj7A1EY5yE9maGl');
+
